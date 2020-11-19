zuarbase/snowflake-sqlalchemy

commit 28e80317968b51da401803fb98515e5e70a35885

SNOW-120653: Fixed string conversion of Snowflake sqlalchemy datatypes

base.py
@@ -376,73 +376,62 @@ class SnowflakeDDLCompiler(compiler.DDLCompiler):
 
 
 class SnowflakeTypeCompiler(compiler.GenericTypeCompiler):
-    def visit_BYTEINT(self, type, **kw):
+    def visit_BYTEINT(self, type_, **kw):
         return "BYTEINT"
 
-    def visit_CHARACTER(self, type, **kw):
+    def visit_CHARACTER(self, type_, **kw):
         return "CHARACTER"
 
-    def visit_DEC(self, type, **kw):
+    def visit_DEC(self, type_, **kw):
         return "DEC"
 
-    def visit_DOUBLE(self, type, **kw):
+    def visit_DOUBLE(self, type_, **kw):
         return "DOUBLE"
 
-    def visit_FIXED(self, type, **kw):
+    def visit_FIXED(self, type_, **kw):
         return "FIXED"
 
-    def visit_INT(self, type, **kw):
+    def visit_INT(self, type_, **kw):
         return "INT"
 
-    def visit_NUMBER(self, type, **kw):
+    def visit_NUMBER(self, type_, **kw):
         return "NUMBER"
 
-    def visit_STRING(self, type, **kw):
+    def visit_STRING(self, type_, **kw):
         return "STRING"
 
-    def visit_TINYINT(self, type, **kw):
+    def visit_TINYINT(self, type_, **kw):
         return "TINYINT"
 
-    def visit_VARIANT(self, type, **kw):
+    def visit_VARIANT(self, type_, **kw):
         return "VARIANT"
 
-    def visit_ARRAY(self, type, **kw):
+    def visit_ARRAY(self, type_, **kw):
         return "ARRAY"
 
-    def visit_OBJECT(self, type, **kw):
+    def visit_OBJECT(self, type_, **kw):
         return "OBJECT"
 
-    def visit_BLOB(self, type, **kw):
+    def visit_BLOB(self, type_, **kw):
         return "BINARY"
 
-    def visit_datetime(self, type, **kw):
-        return self.visit_TIMESTAMP(type, **kw)
+    def visit_datetime(self, type_, **kw):
+        return "datetime"
 
-    def visit_DATETIME(self, type, **kw):
-        return self.visit_TIMESTAMP(type, **kw)
+    def visit_DATETIME(self, type_, **kw):
+        return "DATETIME"
 
-    def visit_TIMESTAMP_NTZ(self, type, **kw):
-        kw['timezone'] = False
-        return self.visit_TIMESTAMP(type, **kw)
+    def visit_TIMESTAMP_NTZ(self, type_, **kw):
+        return "TIMESTAMP_NTZ"
 
-    def visit_TIMESTAMP_TZ(self, type, **kw):
-        kw['timezone'] = True
-        return self.visit_TIMESTAMP(type, **kw)
+    def visit_TIMESTAMP_TZ(self, type_, **kw):
+        return "TIMESTAMP_TZ"
 
-    def visit_TIMESTAMP_LTZ(self, type, **kw):
-        kw['timezone'] = True
-        kw['is_local'] = True
-        return self.visit_TIMESTAMP(type, **kw)
+    def visit_TIMESTAMP_LTZ(self, type_, **kw):
+        return "TIMESTAMP_LTZ"
 
-    def visit_TIMESTAMP(self, type, **kw):
-        is_local = kw.get('is_local', False)
-        timezone = kw.get('timezone', type.timezone)
-        return "TIMESTAMP %s%s" % (
-            (timezone and "WITH" or "WITHOUT") + (
-                    is_local and " LOCAL" or "") + " TIME ZONE",
-            "(%d)" % type.precision if getattr(type, 'precision',
-                                               None) is not None else ""
-        )
+    def visit_TIMESTAMP(self, type_, **kw):
+        return "TIMESTAMP"
 
 
 construct_arguments = [

custom_types.py
@@ -18,25 +18,32 @@ TINYINT = sqltypes.SMALLINT
 VARBINARY = sqltypes.BINARY
 
 
-class VARIANT(sqltypes.TypeEngine):
+class SnowflakeType(sqltypes.TypeEngine):
+
+    def _default_dialect(self):
+        # Get around circular import
+        return __import__('snowflake.sqlalchemy').sqlalchemy.dialect()
+
+
+class VARIANT(SnowflakeType):
     __visit_name__ = 'VARIANT'
 
 
-class OBJECT(sqltypes.TypeEngine):
+class OBJECT(SnowflakeType):
     __visit_name__ = 'OBJECT'
 
 
-class ARRAY(sqltypes.TypeEngine):
+class ARRAY(SnowflakeType):
     __visit_name__ = 'ARRAY'
 
 
-class TIMESTAMP_TZ(sqltypes.TIMESTAMP):
+class TIMESTAMP_TZ(SnowflakeType):
     __visit_name__ = 'TIMESTAMP_TZ'
 
 
-class TIMESTAMP_LTZ(sqltypes.TIMESTAMP):
+class TIMESTAMP_LTZ(SnowflakeType):
     __visit_name__ = 'TIMESTAMP_LTZ'
 
 
-class TIMESTAMP_NTZ(sqltypes.TIMESTAMP):
+class TIMESTAMP_NTZ(SnowflakeType):
     __visit_name__ = 'TIMESTAMP_NTZ'

test/test_compiler.py
@@ -71,5 +71,5 @@ def test_quoted_name_label(engine_testaccount):
     for t in test_cases:
         col = column('colname').label(t["label"])
         sel_from_tbl = select([col]).group_by(col).select_from(table('abc'))
-        compiled_result = sel_from_tbl.compile(engine_testaccount)
+        compiled_result = sel_from_tbl.compile()
         assert str(compiled_result) == t["output"]

test/test_custom_types.py
@@ -1,0 +1,30 @@
+from snowflake.sqlalchemy import custom_types
+
+
+def test_string_conversions():
+    """Makes sure that all of the Snowflake SQLAlchemy types can be turned into Strings"""
+    sf_custom_types = [
+        'VARIANT',
+        'OBJECT',
+        'ARRAY',
+        'TIMESTAMP_TZ',
+        'TIMESTAMP_LTZ',
+        'TIMESTAMP_NTZ',
+    ]
+    sf_types = [
+        'TEXT',
+        'CHARACTER',
+        'DEC',
+        'DOUBLE',
+        'FIXED',
+        'NUMBER',
+        'BYTEINT',
+        'STRING',
+        'TINYINT',
+        'VARBINARY',
+    ] + sf_custom_types
+
+    for type_ in sf_types:
+        sample = getattr(custom_types, type_)()
+        if type_ in sf_custom_types:
+            assert type_ == str(sample)

commit e180b233d0721dc0b76a9f4796ee808aedcabca5

SNOW-227101: fix to datetime columns being created without timezone (TIMESTAMP_NTZ). Commit 28e8031 introduced a bug where all sqlalchemy.DateTime(timezone=True) columns were created as DATETIME in Snowflake which is an alias to TIMESTAMP_NTZ. This commit reverts datetime logic back so if the timezone is defined in datetime column it will create TIMESTAMP_TZ and TIMESTAMP_NTZ otherwise.

base.py
@@ -419,10 +419,10 @@ class SnowflakeTypeCompiler(compiler.GenericTypeCompiler):
         return "BINARY"
 
     def visit_datetime(self, type_, **kw):
-        return "datetime"
+        return self.visit_TIMESTAMP(type_, **kw)
 
     def visit_DATETIME(self, type_, **kw):
-        return "DATETIME"
+        return self.visit_TIMESTAMP(type_, **kw)
 
     def visit_TIMESTAMP_NTZ(self, type_, **kw):
         return "TIMESTAMP_NTZ"
@@ -434,8 +434,14 @@ class SnowflakeTypeCompiler(compiler.GenericTypeCompiler):
         return "TIMESTAMP_LTZ"
 
     def visit_TIMESTAMP(self, type_, **kw):
-        return "TIMESTAMP"
-
+        is_local = kw.get('is_local', False)
+        timezone = kw.get('timezone', type_.timezone)
+        return "TIMESTAMP %s%s" % (
+            (timezone and "WITH" or "WITHOUT") + (
+                    is_local and " LOCAL" or "") + " TIME ZONE",
+            "(%d)" % type_.precision if getattr(type_, 'precision',
+                                               None) is not None else ""
+        )
 
 construct_arguments = [
     (Table, {

test/test_timestamp.py
@@ -9,7 +9,7 @@ from datetime import datetime
 import pytz
 from parameters import CONNECTION_PARAMETERS
 from snowflake.sqlalchemy import TIMESTAMP_LTZ, TIMESTAMP_NTZ, TIMESTAMP_TZ
-from sqlalchemy import Column, Integer, MetaData, Table
+from sqlalchemy import Column, Integer, MetaData, Table, DateTime
 from sqlalchemy.sql import select
 
 PST_TZ = "America/Los_Angeles"
@@ -33,7 +33,10 @@ def test_create_table_timestamp_datatypes(engine_testaccount):
         Column('id', Integer, primary_key=True),
         Column('tsntz', TIMESTAMP_NTZ),
         Column('tsltz', TIMESTAMP_LTZ),
-        Column('tstz', TIMESTAMP_TZ))
+        Column('tstz', TIMESTAMP_TZ),
+        Column('dttz', DateTime(timezone=True)),
+        Column('dtntz', DateTime(timezone=False))
+    )
     metadata.create_all(engine_testaccount)
     try:
         assert test_timestamp is not None
@@ -53,7 +56,10 @@ def test_inspect_timestamp_datatypes(engine_testaccount):
         Column('id', Integer, primary_key=True),
         Column('tsntz', TIMESTAMP_NTZ),
         Column('tsltz', TIMESTAMP_LTZ),
-        Column('tstz', TIMESTAMP_TZ))
+        Column('tstz', TIMESTAMP_TZ),
+        Column('dttz', DateTime(timezone=True)),
+        Column('dtntz', DateTime(timezone=False)),
+    )
     metadata.create_all(engine_testaccount)
     try:
         current_utctime = datetime.utcnow()
@@ -70,6 +76,8 @@ def test_inspect_timestamp_datatypes(engine_testaccount):
             tsntz=current_utctime,
             tsltz=current_localtime,
             tstz=current_localtime_with_other_tz,
+            dttz=current_localtime_with_other_tz,
+            dtntz=current_utctime,
         )
         results = engine_testaccount.execute(ins)
         results.close()
@@ -84,5 +92,7 @@ def test_inspect_timestamp_datatypes(engine_testaccount):
         assert rows[1] == current_utctime
         assert rows[2] == current_localtime
         assert rows[3] == current_localtime_with_other_tz
+        assert rows[4] == current_localtime_with_other_tz
+        assert rows[5] == current_utctime
     finally:
         test_timestamp.drop(engine_testaccount)